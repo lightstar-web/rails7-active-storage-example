2ec3c7d82076e21e8d7233423d0be67074a14299
lightstar-web <dev.star.man0412@gmail.com>
2024-03-26 20:55:11 +0900

    complete.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  # has_one_attached :avatar
+   has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
+  def avatar_url(style: :small)
+    puts "=======avatar url========="
+    puts self.avatar
+    return "" unless self.avatar.attached?
+
+    Rails.application.routes.url_helpers.upload_path(self.avatar.blob.key, s: style)
+  end
 end
